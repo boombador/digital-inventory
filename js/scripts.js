6a8e0e59d8d37aafa0977275903661bbbc965462
Ian McLaughlin <ian.p.mclaughlin@gmail.com>
2019-05-27 20:12:35 -0400

    add stub files for js and css

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+const samples = [
+  { amount: 10, category: 'loans', description: 'jimmy helped me out' },
+  { amount: -5.50, category: 'food', description: 'delicious sammich' },
+  { amount: -3.55, category: 'loans', description: 'singular gallon o\' gasoline' },
+  { amount: -10, category: 'loans', description: 'paid jimmy back' },
+];
+
+function main() {
+  console.log('Starting the main function');
+}
+
+main();
