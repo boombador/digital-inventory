a4c6c5de63712d945d067f4c7fe6e6558a9b10cb
Ian McLaughlin <ian.p.mclaughlin@gmail.com>
2020-03-23 14:18:23 -0400

    Prepare for first exercises

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,47 @@
 import React from "react";
 import Layout from "@/components/Layout";
+import { combinedTotal, projectedSpend, calculateDayOfMonth, calculateDaysInMonth } from "@/features/accounting";
 
-const Home = () => (
-  <Layout>
-    <div>Home page goes here!</div>
-  </Layout>
-);
+const transactionsSoFar = [
+  { amount: -10, description: 'Sandwich' },
+  { amount: -20, description: 'Some soup and a sandwich' },
+  { amount: -60, description: 'Hot new videogame' },
+];
+
+// Fake data for now
+// TODO: pull from redux store instead
+const hardcodedSampleData = {
+  income: 1000,
+  monthProgress: { currentDay: 15, daysInMonth: 30 },
+};
+
+const Home = () => {
+  const { income } = hardcodedSampleData;
+  const { currentDay, daysInMonth } = hardcodedSampleData.monthProgress;
+  return (
+    <Layout>
+      <h2>Metadata</h2>
+      <div className="income">Income: {income}</div>
+      <div className="month-progress">
+        <span>Day {currentDay}</span>&nbsp;
+      <span>of {daysInMonth}</span>
+      </div>
+
+      <h2>Calculated Values</h2>
+      <div id="calculation-results">
+        <div className="projected-spend-daily">Expected daily expenditure: {projectedSpend(income, daysInMonth)}</div>
+        <div className="projected-spend-mtd">Expected expenditures: {projectedSpend(income, currentDay, daysInMonth)}</div>
+        <div className="actual-spend">Real expenditures: {combinedTotal(transactionsSoFar)}</div>
+      </div>
+
+      <h2>Transactions</h2>
+      <div className="transaction-listing">
+        {transactionsSoFar.map((transaction, i) =>
+          <div key={i}>{JSON.stringify(transaction)}</div>
+        )}
+      </div>
+    </Layout>
+  )
+};
 
 export default Home;
--- src/features/accounting.js
+++ src/features/accounting.js
@@ -1,7 +1,27 @@
-import { logAndReturn } from "@/util/debug";
+// import { logAndReturn } from "@/util/debug";
 
-export const balanceFor = transactions =>
-  transactions
-    .map(logWithLabelAndReturn("transaction"))
-    .map(t => t.amount)
-    .reduce((total, amount) => total + amount, 0);
+// not really accounting but leaving in this file for convenience (also, these
+// are more challenging than the accounting functions)
+export const calculateDayOfMonth = timestamp => {
+  return 15;
+}
+export const calculateDaysInMonth = timestamp => {
+  return 30;
+}
+
+// main accounting functions
+
+// add up the amount from each transaction
+export const combinedTotal = transactions => {
+  return 0;
+}
+
+// calculate the expected spend given the assumption that income (post-tax) is
+// equally split between each day in the month
+export const projectedSpend = (income, dayOfMonth, daysInMonth) => {
+  return income;
+}
+
+export const projectedDailySpend = (income, daysInMonth) => {
+  return 0;
+}
